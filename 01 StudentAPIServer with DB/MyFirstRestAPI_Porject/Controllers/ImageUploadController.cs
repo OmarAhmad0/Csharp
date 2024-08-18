753d56cf9e83d4c68488b457b5d3bcd1268a9338
Omar <omar4488.oa@gmail.com>
2024-08-18 22:28:52 +0300

    Add 01 StudentAPIServer with DB And 02 StudentApiClient with DB

---
+++ 01 StudentAPIServer with DB/MyFirstRestAPI_Porject/Controllers/ImageUploadController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentApi.Controllers
+{
+    [Route("api/ImageUpload")]
+    [ApiController]
+    public class ImageUploadController : ControllerBase
+    {
+        [HttpPost("Upload")]
+        public async Task<IActionResult> UploadImage(IFormFile imageFile)
+        {
+            if (imageFile == null || imageFile.Length == 0) return BadRequest("No File Uploaded");
+            var uploadDirectory = @"C:\MyUpload";
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+            var filePath = Path.Combine(uploadDirectory, fileName);
+
+            if (!Directory.Exists(uploadDirectory))
+                Directory.CreateDirectory(uploadDirectory);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(stream);
+            }
+            return Ok(new { filePath });
+        }
+
+        [HttpGet("getImage/{fileName}")]
+        public IActionResult GetImage(string fileName)
+        {
+            var uploadDirectory = @"C:\MyUpload";
+            var filePath = Path.Combine(uploadDirectory, fileName);
+
+            if (!System.IO.File.Exists(filePath))
+                return NotFound("Image Not Found");
+
+            var image = System.IO.File.OpenRead(filePath);
+            var mimeType = GetMimeType(filePath);
+
+
+            return File(image, mimeType);
+        }
+
+        private string GetMimeType(string filePath)
+        {
+            var extn = Path.GetExtension(filePath).ToLowerInvariant();
+
+            return extn switch
+            {
+                ".jpg" or ".jpeg" => "image/jpeg",
+                ".png" => "image/png",
+                ".gif" => "image/gif",
+               _ => "application/octet-stream",
+
+            };
+
+        }
+
+    }
+}
---
+++ 01 StudentAPIServer with DB/StudentDataAccessLayer/clsDataSettings.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+
+namespace StudentDataAccessLayer
+{
+    public class clsDataSettings
+    {
+        public  static string? _connectionString;
+
+        public static void Initilize(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+ 
+    }
+}
